f9aaa3510e44a7926fef8972b3a9dcee47e0550d
Enguerran <engcolson@gmail.com>
2014-03-30 09:42:33 +0200

    add some tests and enhance the demo

--- dist/b-flash-message.js
+++ dist/b-flash-message.js
@@ -1,30 +1,30 @@
-(function() {
-    
-
-    Bosonic.registerElement(
-        'b-flash-message',
-        {
-    readyCallback: function () {
-        var root = this.createShadowRoot();
-        root.appendChild(this.template.content.cloneNode(true));
-        var isCloseable = this.getAttribute('closeable') !== 'false';
-        var closeButton = root.querySelector('button.close');
-        if (isCloseable)
-            closeButton.addEventListener('click', this.close.bind(this));
-        else
-            closeButton.remove();
-    },
-    show: function () {
-        this.setAttribute('visible', '');
-        this.classList.add(this.getAttribute('type'));
-        var duration = this.getAttribute('duration');
-        if (duration && !isNaN(parseInt(duration)))
-            setTimeout(this.close.bind(this), duration);
-    },
-    close: function () {
-        this.removeAttribute('visible');
-    },
-    template: '        <div class="b-flash-message alert-closeable">        \t<button type="button" class="close">\xD7</button>            <content></content>        </div>    '
-}
-    );
+(function () {
+    Bosonic.registerElement('b-flash-message', {
+        readyCallback: function () {
+            var root = this.createShadowRoot();
+            root.appendChild(this.template.content.cloneNode(true));
+            var isCloseable = this.getAttribute('closeable') === 'true';
+            var closeButton = root.querySelector('button.close');
+            if (isCloseable)
+                closeButton.addEventListener('click', this.close.bind(this));
+            else
+                closeButton.remove();
+            var type = this.getAttribute('type');
+            if (!type)
+                this.setAttribute('type', 'info');
+            var isVisible = this.getAttribute('visible') === 'true';
+            if (isVisible)
+                this.show();
+        },
+        show: function () {
+            this.setAttribute('visible', 'true');
+            var duration = this.getAttribute('duration');
+            if (duration && !isNaN(parseInt(duration)))
+                setTimeout(this.close.bind(this), duration);
+        },
+        close: function () {
+            this.removeAttribute('visible');
+        },
+        template: ' <div class="b-flash-message alert-closeable"> <button type="button" class="close">\xD7</button> <content></content> </div> '
+    });
 }());
--- test/b-flash-message.js
+++ test/b-flash-message.js
@@ -1,5 +1,46 @@
-describe("b-flash-message", function() {
-    it("should XXX", function() {
+// var mouse = effroi.mouse;
 
+describe("b-flash-message", function() {
+    "use strict";
+    it("should be hidden by default", function() {
+        var flashMessage = document.createElement('b-flash-message');
+        expect(flashMessage.getAttribute('visible')).to.be.null;
+        //expect(window.getComputedStyle(flashMessage).display).to.equal('none');
+    });
+
+    it("should be visible when calling show()", function() {
+        var flashMessage = document.createElement('b-flash-message');
+        flashMessage.show();
+        expect(flashMessage.getAttribute('visible')).to.equal('true');
+        //expect(window.getComputedStyle(flashMessage).display).to.equal('block');
+    });
+
+    it("should be closed when calling close()", function() {
+        var flashMessage = document.createElement('b-flash-message');
+        flashMessage.setAttribute('isCloseable', 'true');
+        flashMessage.show();
+        expect(flashMessage.getAttribute('visible')).to.equal('true');
+        //mouse.click(flashMessage.querySelector('button.close'));
+        flashMessage.close();
+        expect(flashMessage.getAttribute('visible')).to.be.null;
+    });
+
+    it("should be closed automatically after a certain duration", function(done) {
+        var flashMessage = document.createElement('b-flash-message');
+        flashMessage.setAttribute('duration', 5);
+        flashMessage.show();
+        expect(flashMessage.getAttribute('visible')).to.equal('true');
+        setTimeout(function() {
+            expect(flashMessage.getAttribute('visible')).to.be.null;
+            done();
+        }, 10);
+    });
+
+    it("should be stylized by type", function() {
+        var flashMessage = document.createElement('b-flash-message');
+        expect(flashMessage.getAttribute('type')).to.equal('info');
+        // expect(flashMessage.style.backgroundColor).to.equal('#d9edf7');
+        flashMessage.setAttribute('type', 'error');
+        // expect(flashMessage.style.backgroundColor).to.equal('#f2dede');
     });
 });
